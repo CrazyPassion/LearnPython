4b8fae0d6daf06a5c3c3f7ae2fd3501e495ce992
CrazyPassion <von.king2010@gmail.com>
2017-11-01 21:10:38 +0800

    update book.py

--- Book.py
+++ Book.py
@@ -1,9 +1,50 @@
 # Book.py
 
-def price():
-1233
+def findMinCost(Y1, Y2, Y3, Y4, Y5):
+    arr =[Y1, Y2, Y3, Y4, Y5]
+    # print arr
+    arr.sort()
+    # print arr
+    if arr[0]<0:
+        print "the book number shoulb bigger than 0"
+        return 0
+    #Y1>=Y2>=Y3>=Y4>=Y5
+    Y5=arr[0]
+    Y4=arr[1]
+    Y3=arr[2]
+    Y2=arr[3]
+    Y1=arr[4]
+    cost = 0.0
+    if (Y5>=1):
+        cost=min(
+                8*5*(1-0.25)+findMinCost(Y1-1,Y2-1,Y3-1,Y4-1,Y5-1),
+                8*4*(1-0.20)+findMinCost(Y1-1,Y2-1,Y3-1,Y4-1,Y5),
+                8*3*(1-0.10)+findMinCost(Y1-1,Y2-1,Y3-1,Y4,Y5),
+                8*2*(1-0.05)+findMinCost(Y1-1,Y2-1,Y3,Y4,Y5)
+            )
+    elif (Y4>=1):
+        cost=min(
+                8*4*(1-0.20)+findMinCost(Y1-1,Y2-1,Y3-1,Y4-1,Y5),
+                8*3*(1-0.10)+findMinCost(Y1-1,Y2-1,Y3-1,Y4,Y5),
+                8*2*(1-0.05)+findMinCost(Y1-1,Y2-1,Y3,Y4,Y5)
+            )
+    elif(Y3>=1):
+        cost=min(
+                8*3*(1-0.10)+findMinCost(Y1-1,Y2-1,Y3-1,Y4,Y5),
+                8*2*(1-0.05)+findMinCost(Y1-1,Y2-1,Y3,Y4,Y5)
+            )
+    elif(Y2>=1):
+        cost=8*2*(1-0.05)+findMinCost(Y1-1,Y2-1,Y3,Y4,Y5)
+    elif(Y1>=1):#{Y1,0,0,0,0} #same book
+        cost=8*Y1
+    else:#0,0,0,0,0
+        cost=0
+    return cost
 
 
 if __name__ == '__main__':
-	print "start"
+    print "start"
+    print findMinCost(3,2,1,4,5)
+    print findMinCost(2,2,2,1,1) #51.2
+    print findMinCost(1,2,3,3,0) #29.6
 
--- BowlingGame.py
+++ BowlingGame.py
@@ -1,16 +1,49 @@
 import re
 
-def CalculateScore(file):
-	print -9876543210 + (-1234567890)
+def CalculateAllScore(file_path):
+    fp = open(file_path, 'r')
+    linenum = 0
+    caseNum = 0
+    for line in fp:
+        print line
+        if (linenum == 0):
+            # totalCaseNum =
+            # print totalCaseNum
+            pass
+        else:
+            score = CalcScore(line)
+            print "Case #" + str(caseNum) +": "+str(score)
+
+        linenum = linenum + 1
+
+        print "line" +str(linenum)
+        if linenum == 1:
+            break
+    fp.close()
 
 
-line ='1/|4-|63|61|22|8-|3/|4/|-/|6/||-'
+def testcode():
+    l1 = "X|X|X|X|X|X|X|X|X|X||XX" #300
+    l2 = "X|7/|9-|X|-8|8/|-6|X|X|X||81"#167
+    # print "l1",CalcScore(l1)
+    print "l2",CalcScore(l2)
 
-def getAllRound(line):
-	# matchgroup=re.
-	pass
+def CalcScore(line):
+    mg=re.match(r"(\w|\d?|\S)\|(\w|\d?|\S)\|(\w|\d?\S)\|(\w|\d?\S)\|(\w|\d?\S)\|(\w|\d?\S)\|(\w|\d?\S)\|(\w|\d?\S)\|(\w|\d?\S)\|(\w|\d?\S)\|\|(\w+|\d+).*",line)
+    print line
+    length = len(mg.groups())
+    print length
+    tmplist = []
+    for i in xrange(1,length):
+        # print mg.group(i)
+        tmplist.append(mg.group(i))
 
-	
+    print tmplist
+    print "end"
+    return 0
+
+
+
 if __name__ == '__main__':
-
-	CalculateScore(file)
+    testcode()
+    # CalculateAllScore(file)
